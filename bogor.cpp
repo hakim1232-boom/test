5a6040a3cd86e1b09d5a594680fe0cae5a3f439e
hakim1232-boom <hakim011205@gmail.com>
2026-02-16 12:28:28 +0700

    Add files via upload

--- bogor.cpp
+++ bogor.cpp
@@ -4,29 +4,23 @@ using namespace std;
 
 int main() {
     const double PI = 3.14159265359;
-    double diameter_ban_cm;
-    double jarak_km;
-    double keliling_ban_cm;
-    double jarak_cm;
-    double total_putaran;
+    double diameter_ban_cm,jarak_km,keliling_ban_cm,jarak_cm,total_putaran;
 
-    // Input data
-    cout << "Masukkan diameter ban motor (cm): ";
+    cout << "Masukkan diameter ban motornya (cm): ";
     cin >> diameter_ban_cm ;
 
-    cout << "Masukkan jarak tempuh (km): ";
+    cout << "Masukkan jarak tempuhnya (km): ";
     cin >> jarak_km;
 
-    // hitung keliling ban
     keliling_ban_cm = PI * diameter_ban_cm;
     jarak_cm = jarak_km * 100000;
     total_putaran = jarak_cm / keliling_ban_cm;
 
-    // Output
-    cout << "\n=== HASIL PERHITUNGAN ===" << endl;
+    
+    cout << "HASIL PUTARAN" << endl;
     cout << "Keliling ban        : " << keliling_ban_cm << " cm" << endl;
     cout << "Jarak tempuh        : " << jarak_cm << " cm" << endl;
     cout << "Total putaran roda  : " << total_putaran << " putaran" << endl;
 
     return 0;
-}
+} 
